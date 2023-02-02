566923b9259e730a9a57e1d2611cda1ee20fa1d6
ecyht2 <ecyht2@nottingham.edu.my>
2023-02-02 11:07:18 +0800

    Added operator overloading to print to an osstream

--- src/big-text/big-text.h
+++ src/big-text/big-text.h
@@ -38,12 +38,12 @@ class BigText {
      *
      * @param stream The stream to print to.
      * */
-    void print(ostream &stream = std::cout);
+    void print(ostream &stream = std::cout) const;
     /** Gets the text currently text stored in the class.
      *
      * @return The current stored text.
      * */
-    string getText(void);
+    string getText(void) const;
     /** Sets the text currently to print.
      *
      * @param str New string to set to.
@@ -54,11 +54,17 @@ class BigText {
      *
      * @return A string containing all supported chracters.
      * */
-    string getSupportedCharacters(void);
+    string getSupportedCharacters(void) const;
     /** Sets the big_text_map to use when printing. */
     void setMap(big_text_map *map);
     /** Gets the big_text_map to currenlty being used when printing. */
-    big_text_map& getMap(void);
+    big_text_map& getMap(void) const;
+    /** Operator overloading to print the store string to the output stream.
+     *
+     * @param out The stream to print to.
+     * @param cls The BitText class to print.
+     * */
+    friend ostream& operator << (ostream &out, const BigText &cls);
   private:
     /** String currently stored. */
     string text;
